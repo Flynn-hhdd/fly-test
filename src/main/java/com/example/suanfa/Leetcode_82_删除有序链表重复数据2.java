3f87ea7aeaac9d9cdae53b69459df34fc2d784b6
fanlingyin <fanlingyin@didiglobal.com>
2021-10-26 20:53:39 +0800

    push

--- src/main/java/com/example/suanfa/Leetcode_82_删除有序链表重复数据2.java
+++ src/main/java/com/example/suanfa/Leetcode_82_删除有序链表重复数据2.java
@@ -1,4 +1,39 @@
 package com.example.suanfa;
 
+import lombok.val;
+
+import java.util.List;
+
 public class Leetcode_82_删除有序链表重复数据2 {
+     class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next==null){
+            return head;
+        }
+        //前提整体是个顺序链表
+        //保留头节点，后面继续递归
+        if(head.val != head.next.val){
+            head.next = deleteDuplicates(head.next);
+        }else {
+            ListNode tmp = head.next;
+            while (tmp!=null && tmp.val==head.next.val){
+                tmp = tmp.next;
+            }
+            head = deleteDuplicates(tmp);
+        }
+
+        return head;
+    }
 }
--- src/main/java/com/example/suanfa/Leetcode_最长子回文串.java
+++ src/main/java/com/example/suanfa/Leetcode_最长子回文串.java
@@ -8,4 +8,25 @@ package com.example.suanfa;
  * @createTime 2020年04月21日 17:35:00
  */
 public class Leetcode_最长子回文串 {
+
+    public String longestPalindrome(String s) {
+        char[] array =  s.toCharArray();
+        if(array.length==1){
+            return s;
+        }
+
+        return "";
+    }
+
+    //暴力解法1
+    public String longestPalindrome1(String s) {
+        char[] array =  s.toCharArray();
+        if(array.length==1){
+            return s;
+        }
+
+        return "";
+    }
+
+
 }
